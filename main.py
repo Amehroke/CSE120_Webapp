1a38cba958175be944c2637dc2f5613451733902
Amehroke <arashdeepsingh123@gmail.com>
2023-10-27 22:59:10 -0700

    Arranged image positions

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import base64
-
-
-def get_image_base64(image_path):
-    with open(image_path, "rb") as img_file:
-        return base64.b64encode(img_file.read()).decode('utf-8')
 
 
 # Insert containers separated into tabs:
